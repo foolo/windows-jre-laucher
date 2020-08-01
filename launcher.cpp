8e099e8a5209648ffb7a8f878b0900e222d16ced
Olof Andersson <olof.andersson@gmail.com>
2020-08-01 09:30:48 +0200

    launcher

---
+++ launcher.cpp
@@ -0,0 +1,56 @@
+#include <windows.h>
+#include <sstream>
+#include <shlwapi.h>
+
+void cdToApplication() {
+	wchar_t path[MAX_PATH];
+	int len = GetModuleFileNameW(NULL, path, MAX_PATH);
+	if (len > 0 && len < MAX_PATH) {
+		path[len-1] = 0;
+		if (PathRemoveFileSpecW(path) != 0) {
+			SetCurrentDirectoryW(path);
+		}
+	}
+}
+
+int main() {
+	cdToApplication();
+	STARTUPINFOW si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	LPCWSTR cmd = L"jre\\bin\\java.exe";
+	WCHAR args[] = L"jre\\bin\\java.exe --class-path \"lib/*\" my.package.MyApplication";
+	if (CreateProcessW(cmd, args, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi) == 0) {
+		std::wstringstream ss;
+		ss << "Could not run " << cmd << std::endl;
+		ss << "error code: " << GetLastError() << std::endl;
+		ss << "command line: " << args << std::endl;
+		MessageBoxExW(NULL, ss.str().c_str(), L"Message", MB_OK | MB_ICONERROR, 0);
+		return 0;
+	}
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	DWORD exitCode = 0;
+	if (GetExitCodeProcess(pi.hProcess, &exitCode) != 0) {
+		if (exitCode != 0) {
+			std::wstringstream ss;
+			ss << cmd << std::endl;
+			ss << "failed with exit code " << exitCode << std::endl;
+			ss << "command line: " << args << std::endl;
+			MessageBoxExW(NULL, ss.str().c_str(), L"Message", MB_OK | MB_ICONERROR, 0);
+		}
+	}
+	else {
+		std::wstringstream ss;
+		ss << cmd << std::endl;
+		ss << "terminated with unknown exit code" << std::endl;
+		ss << "command line: " << args << std::endl;
+		MessageBoxExW(NULL, ss.str().c_str(), L"Message", MB_OK | MB_ICONERROR, 0);
+	}
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	return 0;
+}
